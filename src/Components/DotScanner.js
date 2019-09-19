5d0834b0b83dd46648b37ede5fa4deffed6be00b
Mohit Kumar <mohitk@geekyants.com>
2019-09-19 13:05:40 +0530

    error feature while scanning

--- src/Components/DotScanner.js
+++ src/Components/DotScanner.js
@@ -7,11 +7,16 @@ import {
   CameraAccess,
   CameraSettings,
   ScanSettings,
-  SymbologySettings
+  SymbologySettings,
+  Camera,
+  BrowserCompatibility,
+  CustomError
 } from "scandit-sdk";
 
+const engineLocation = "https://cdn.jsdelivr.net/npm/scandit-sdk@4.x/build";
 const licenseKey =
   "AeN9zl8OQZ7rGTtUQTWc5yIXcjRVLvW9DWqSIHYfMM+8L8WiJ0juJcRtHI2GUJq5kUQAn9cCpXgXXixaAmq5llBOGGOmWZGOM3fagS411ISPQZ/+hiQ2MO9Ez2vrTOtjEc2BsPDm12skZVrOJV6S5bmTQRnPEkcGsnVEfmgP4yw7HiXq7bo6DzPOXpLxxrofqLfmsaOeaCG7OZiPe/nq5cSSwdBWTQeWxXBfy/J6o45NkAmJ/NqOb+t6wvYks1Ak2gFIIFsn7dPGWNSxfyeqIk7ufq5tRHL8Y6Mk2RTYVe4ME094g5h03fLnS6dZxfA8NJrwOv8IDswAo5Lgn5pRZri4JAPrmGpd5E15JBh3f22q2K7lbl/eQVBW1rA6pfM2wKTUsie8YZaqmw9pxGqQl8TdXDwUoWDo+f8Pbj3QKWrebHpXZA+AI2ICyrqmpkWIk5q+mSmUx/SxIWgilREjOrisk8kHcMZdwOBb2UV+POos43t7PJpvcdcBtdptUoKAyPLNpZ4ftJ5MP8lFSH4xYt9eNA0O5Xdfgb0s8QXF5NAr4ej9GeNwR5RAEQ122oHeDdx95kXTPMGZFjrTEgGLnkuOG1qFl+exLIrhfzwx3SoXe6uGNgsphuNUziNAQ7FnqLy9uGCAXWmnnGsgYnOGqq27PbjNl7EFiKa0pb5gyDO9EDuFyNP4jgijxCGcWYsmzSCIuRV3ANG2NBdhrA58fb81Dq89BNwim6M+dCTXqSvYyn1+GxMLeYF4qq1IScc6ln9yDX6Y3nGfyIKnxLJTAKjJyrWeqhPCP1du";
+let timeOut = 0;
 class DotScanner extends React.Component {
   constructor(props) {
     super(props);
@@ -19,11 +24,19 @@ class DotScanner extends React.Component {
       data: "No data found!",
       scannerReady: false,
       enableTorchToggle: true,
-      paused: false
+      error: false
     };
   }
+
+  componentDidMount = () => {
+    CameraAccess.getCameras()
+      .then(res => console.log("camera response", res))
+      .catch(err => console.log("camera error", err));
+  };
+
   getScanSettings = () => {
     let scanObj = new ScanSettings();
+    scanObj.blurryRecognition = true;
     scanObj.setMultipleSymbologiesEnabled(
       [
         Barcode.Symbology.DOTCODE,
@@ -47,35 +60,50 @@ class DotScanner extends React.Component {
   };
 
   handleOnScan = res => {
-    console.log("asdibasjd", res);
+    console.log("scan response", res);
     if (res) this.setState({ data: res.barcodes[0].data });
     else this.setState({ data: "No data found!" });
   };
 
   handleProcessFrame = proc => {
+    if (proc && proc.barcodes.length === 0) {
+      timeOut++;
+      if (timeOut === 50) {
+        alert(
+          "Can't scan Image, either symbology not supported or Image not clear!"
+        );
+        timeOut = 0;
+      }
+    } else {
+      timeOut = 0;
+      alert(proc && proc.barcodes[0].data);
+    }
     console.log("process", proc);
   };
+
+  getCameraSettings = () => {};
 
   render() {
     return (
       <div>
         <ScanditBarcodeScanner
           licenseKey={licenseKey}
-          engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@4.x/build"
+          engineLocation={engineLocation}
           ready={() =>
             this.setState({ scannerReady: true, data: "No data found!" })
-          } // Picker events
+          }
           onScan={res => this.handleOnScan(res)}
           onScanError={err => console.log("error", err)}
-          // onProcessFrame={proc => this.handleProcessFrame(proc)}
-          scanSettings={this.getScanSettings()} // Picker options(Tab/QR/DotCode)
+          onProcessFrame={proc => this.handleProcessFrame(proc)}
+          scanSettings={this.getScanSettings()}
           singleImageMod={true}
           enableTapToFocus={true}
           playSoundOnScan={true}
-          paused={this.state.paused}
+          paused={false}
           visible={true}
+          enableTorchToggle={true}
+          cameraSettings={this.getCameraSettings()}
         />
-        <div>{this.state.data}</div>
       </div>
     );
   }
